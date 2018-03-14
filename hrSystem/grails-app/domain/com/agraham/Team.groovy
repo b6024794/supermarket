e93f1cee06035471ecb6ed3e657f1b60e59b26f1
b6024794 <b6024794@my.shu.ac.uk>
2018-03-14 14:29:36 +0000

    Team controller done

--- hrSystem/grails-app/domain/com/agraham/Team.groovy
+++ hrSystem/grails-app/domain/com/agraham/Team.groovy
@@ -1,7 +1,18 @@
 package com.agraham
 
 class Team {
+	String teamName
+	int noOfEmployees
+	String descript
+	String sectionName
 
     static constraints = {
+
+	teamName blank: false, nullable: false
+	noOfEmployees blank: false, nullable: false
+	sectionName blank: false, nullable: false
+
+
+	
     }
 }
