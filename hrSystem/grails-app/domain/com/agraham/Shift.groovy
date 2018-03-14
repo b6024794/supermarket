7747068b46102d978e3c45baa3932ffac1ee7747
b6024794 <b6024794@my.shu.ac.uk>
2018-03-14 15:00:42 +0000

    all but manger and shift domain clasees finshed

--- hrSystem/grails-app/domain/com/agraham/Shift.groovy
+++ hrSystem/grails-app/domain/com/agraham/Shift.groovy
@@ -2,6 +2,16 @@ package com.agraham
 
 class Shift {
 
+	String timeOfDay
+	String daysOfWeek
+	int numberOfHours
+	String startingTime
+
+
     static constraints = {
+	timeOfDay blank: false, nullable: false
+	daysOfWeek blank: false, nullable: false
+	numberOfHours blank: false, nullable: false, max: 13
+	startingTime blank: false, nullable: false
     }
 }
--- hrSystem/grails-app/domain/com/agraham/Task.groovy
+++ hrSystem/grails-app/domain/com/agraham/Task.groovy
@@ -2,6 +2,22 @@ package com.agraham
 
 class Task {
 
+String taskName
+int numberOfPeople
+String sectionName
+String timeRequired
+String description
+String department
+Boolean taskCompleted
+
+
     static constraints = {
+
+taskName blank: false, nullable: false
+numberOfPeople blank: false, nullable: false
+sectionName blank: false, nullable: false
+timeRequired blank: false, nullable: false
+description blank: false, nullable: false
+taskCompleted blank: false, nullable: false
     }
 }
